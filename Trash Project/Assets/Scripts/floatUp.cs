adabbc8a6b1a3cc3e4f0f0d1c3307ba351ac3dff
Student <student@v314-ent-010.local>
2019-11-05 15:29:22 -0500

    added scripts

---
+++ Trash Project/Assets/Scripts/floatUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatUp : MonoBehaviour
+{
+    float speed = 2;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        MoveUp();
+       
+    }
+
+    void MoveUp()
+    {
+        transform.position += Vector3.up * Time.deltaTime * speed;
+        transform.Rotate(0, 5, 0 * Time.deltaTime);
+    }
+}
